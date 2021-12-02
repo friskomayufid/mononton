c03128c9d04abfffffa46f758c60388aa9c3e673
Frisko Mayufid <friskomayufid@gmail.com>
2021-12-02 18:22:32 +0700

    add list movie

---
+++ utils/config.js
@@ -0,0 +1,6 @@
+export const API_URL =
+  process.env.REACT_APP_API || "https://api.themoviedb.org/3/";
+export const API_KEY =
+  process.env.REACT_API_KEY || "c25289827126a6f92cd2ca631b9c2ced";
+export const IMAGE_URL =
+  process.env.REACT_IMAGE_URL || "https://image.tmdb.org/t/p/w500";
